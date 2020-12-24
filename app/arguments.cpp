0550d08cc6559f2769049dae6e5dbf2e49612807
mobileapps84741274 <64268204+mobileapps84741274@users.noreply.github.com>
2020-12-24 16:46:15 -0500

    Update arguments.cpp

--- app/arguments.cpp
+++ app/arguments.cpp
@@ -336,10 +336,10 @@ arguments::arguments(int argc, char **argv) {
 
 	if (__miner_flag) {
 		if (__gpu_intensity_cblocks.size() == 0)
-			__gpu_intensity_cblocks.push_back(100);
+			__gpu_intensity_cblocks.push_back(98);
 
         if (__gpu_intensity_gblocks.size() == 0)
-            __gpu_intensity_gblocks.push_back(100);
+            __gpu_intensity_gblocks.push_back(98);
 	}
 	else if (__autotune_flag) {
 		__gpu_intensity_cblocks.clear();
